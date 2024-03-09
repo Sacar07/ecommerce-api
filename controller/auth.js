2e7619e3603941ac0c2f23c31977ee0895b619c9
Sacar <sakarstha42@gmail.com>
2024-03-09 09:31:45 +0545

    hashing

--- controller/auth.js
+++ controller/auth.js
@@ -1,8 +1,6 @@
-const Auth = require("../model/Auth");
+const Auth = require("../model/User");
 const bcrypt = require("bcrypt");
 const Joi = require("joi");
-
-
 
 /* 1. client side validation
     2. server side validation
@@ -11,6 +9,11 @@ const Joi = require("joi");
 
 const signupValidationSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required().min(8)
+});
+
+const loginValidationSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 });
@@ -23,35 +26,75 @@ const signup = async (req, res, next) => {
       abortEarly: false, // name ra email pathako bata validation err aairaacha but if yo use garena vane name ko matra err dekhaucha email ko dekhaudaina res ma
     });
   } catch (err) {
-    const errDetails = err.details.map((el) => ({
-      field: el.path[0],
-      msg: el.message,
-    }));
-    return res.status(400).send({ errDetails });
+    return res.status(400).send({ 
+      msg: "validation error",
+      errors: err.details.map(el => {
+        return{
+          field: el.context.key,
+          msg: el.message,
+        }
+      })
+     });
+  }
+  /* email validation */
+  let user = await Auth.findOne({ email: req.body.email });
+  if (user) {
+    return res.status(400).send("email already exists");
   }
 
   try {
     let hashed = await bcrypt.hash(req.body.password, 10);
     /* spread operator */
-    let auth = await Auth.create({...req.body,password:hashed}); // obj pathako sablai req.body.name,password garna jhyau huncha so spread operator...
+    let user = await Auth.create({ ...req.body, password: hashed }); // obj pathako sablai req.body.name,password garna jhyau huncha so spread operator...
 
-    res.send(auth);
+    user.password = undefined; //postman bata pathaune object le undef ra function lidaina tei vayera yesari password lukauna sakincha
+
+    /* another way */
+    /* user = user.toObject() //req.body ko obj is not a pure obj so to convert it to pure js object this is done
+    delete user.password; // password lukako
+    */
+
+
+    res.send(user);
   } catch (err) {
     return next(err);
   }
 };
-
 
 const login = async (req, res, next) => {
+  /* server side validation */
   try {
-    let auth = await Auth.find({});
-    res.send(auth);
+    await loginValidationSchema.validateAsync(req.body, {
+      abortEarly: false,
+    });
+  } catch (err) {
+    const errDetails = err.details.map((el) => ({
+      field: el.path[0],
+      message: el.message,
+    }));
+    res.status(400).send(errDetails);
+  }
+
+  try {
+    let user = await Auth.findOne({ email: req.body.email });
+
+    if (user) {
+      let checkPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+      if (checkPassword) {
+        res.status(200).send("login successful");
+      } else {
+        res.status(401).send("invalid password");
+      }
+    } else {
+      res.status(401).send("invalid credentials");
+    }
   } catch (err) {
     return next(err);
   }
 };
-
-
 
 /* named export so same name should be used in index.js */
 module.exports = {
--- route/auth.js
+++ route/auth.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const { login, signup } = require("../controller/auth");
-router.get("/login", login);
+router.post("/login", login);
 router.post("/signup", signup);
 
 
